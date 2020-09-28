f0264896dec084a88cb890059d8a0d8262805d2c
martingarciafigueira <martin.garcia.figueira@ciclosmontecastelo.com>
2020-09-28 21:56:14 +0200

    Tema 2 y 3

---
+++ Tema03 - Fundamentos de C#/Ejercicios/Tema3/Ejercicio25/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Ejercicio25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int calificacion1 = 39, calificacion2 = 62, notapromedio;
+
+            notapromedio = (calificacion1 + calificacion2) / 2;
+
+            Console.WriteLine("La nota final es: " + notapromedio); //La nota final es 50
+        }
+    }
+}
---
+++ Tema03 - Fundamentos de C#/Ejercicios/Tema3/Ejercicio7/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Ejercicio7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float valorhr, hrstrab, antigue;
+            string nombre;
+
+            Console.Write("Nombre:");
+            nombre = Console.ReadLine();
+
+            Console.Write("Valor hora:");
+            valorhr = float.Parse(Console.ReadLine());
+
+            Console.Write("Antiguedad:");
+            antigue = float.Parse(Console.ReadLine());
+
+            Console.Write("Horas trabajadas:");
+            hrstrab = float.Parse(Console.ReadLine());
+
+            Console.Clear();
+            Console.WriteLine("Nombre: {0}", nombre);
+            Console.WriteLine("Antiguedad: {0}", antigue);
+            Console.WriteLine("Valor hora: {0}", valorhr);
+            Console.WriteLine("Total a cobrar: {0}", valorhr * hrstrab + antigue * 30);
+            Console.WriteLine("Total del descuento: {0}", (valorhr * hrstrab + antigue * 30) * 0.13);
+            Console.WriteLine("Total con descuento: {0}", ((valorhr * hrstrab + antigue * 30) - (valorhr * hrstrab + antigue * 30) * 0.13));
+            Console.Read();
+        }
+    }
+}
