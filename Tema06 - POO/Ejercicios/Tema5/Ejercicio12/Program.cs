27e198cfff560dee104a03d9f9e2d869cacd839a
martingarciafigueira <martin.garcia.figueira@ciclosmontecastelo.com>
2020-11-22 11:41:40 +0100

    Tema6

---
+++ Tema06 - POO/Ejercicios/Tema5/Ejercicio12/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ejercicio12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Tema06 - POO/Ejercicios/Tema5/Ejercicio8/Globo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicio8
+{
+    class Globo
+    {
+        private int x = 50;
+        private int y = 50;
+        private int diametro = 20;
+        private string color = "Blanco";
+    }
+}
