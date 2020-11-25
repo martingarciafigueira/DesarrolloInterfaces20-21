9d789ad026f0f30661c2c2312133123b91305ec7
martingarciafigueira <martin.garcia.figueira@ciclosmontecastelo.com>
2020-11-25 21:59:10 +0100

    Tema 6 y 7

---
+++ Tema07 - Excepciones/Ejercicios/Tema7/Ejercicio1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int aux = int.Parse("Hola");
+
+
+            try
+            {
+                Console.WriteLine("Mensaje mostrado por pantalla");
+                Exception e = new Exception("Esto es un objeto Exception");
+                throw e;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Excepcion capturada con mensaje: " + ex.Message);
+            }
+            Console.WriteLine("Programa terminado");
+        }
+    }
+}
---
+++ Tema07 - Excepciones/Ejercicios/Tema7/Ejercicio2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Generando número aleatorio");
+                Random generador = new Random();
+                int entero = (int)(generador.Next() * 1000);
+                if (entero % 2 == 0)
+                {
+                    throw new Exception("Se generó un numero par");
+                }
+                else
+                {
+                    throw new Exception("Es impar");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
